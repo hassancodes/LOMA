b2d961ea1a49647859bea05e718438dee8390867
Muhammad hassan <hassanhanjra900@gmail.com>
2022-12-27 01:25:05 -0500

    feat: adding flask

--- app.py
+++ app.py
@@ -7,8 +7,20 @@ def home():
     return render_template("home.html")
 
 
+@app.route("/combinations")
+def combinations():
+    # schema for a specific problem
+    prob_data = {
+        "type":"combination",
+        "totalInput" : 2,
+    }
+    return render_template("solve.html", data=prob_data)
 
 
+# this function will calculate and
+@app.route("/calculate/<problemtype>", methods=["POST"])
+def calculate():
+    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
